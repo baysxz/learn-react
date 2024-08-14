fc99f888347b7f598e52fae8338ca5cbfb9671a0
24LP3558@nest.edu.mn <24lp3558nest.edu.mn@G305-CC0035.local>
2024-08-14 10:02:36 +0800

    implemented login page

---
+++ src/components/Button.js
@@ -0,0 +1,4 @@
+import styles from "../styles/Button.module.css";
+export default function Button(props) {
+  return <button className={styles.button}>{props.text}</button>;
+}
---
+++ src/components/Input.js
@@ -0,0 +1,4 @@
+import styles from "../styles/Input.module.css";
+export function Input() {
+  return <input placeholder="First name" className={styles.input} />;
+}
